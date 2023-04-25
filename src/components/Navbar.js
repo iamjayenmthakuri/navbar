09ee4e10fd2d69d1743e5a68776171d709e27872
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-25 18:01:36 +0545

    navbar practice

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../components/Navbar.module.css";
+
+const Navbar = () => {
+  return <div className={styles.main}>navbar</div>;
+};
+
+export default Navbar;
